15345b94c134dafdd24f7c0ff4555d1dcc22f1c8
xhonatankonomi <xhoni8962@gmail.com>
2025-10-14 17:52:56 +0200

    feat: Set lamp image source and alt attribute
    This commit sets the `src` attribute of the lamp image to the correct path and adds an `alt` attribute for accessibility.

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,5 +2,6 @@ const lampImg = document.getElementById("lamp-image");
 const imgPath = './assets/img/yellow_lamp.png'
 
 function changeImage () {
-    
+    lampImg.src = imgPath;
+    lampImg.alt = 'yellow_lamp';
 }
