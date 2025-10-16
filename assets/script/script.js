10aef33ee5bac2ddc5c6fb529de7d03696071548
xhonatankonomi <xhoni8962@gmail.com>
2025-10-16 15:06:50 +0200

    feat: Refactor button to use event listener for image change

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,10 @@
 const lampImg = document.getElementById("lamp-image");
 const imgPath = './assets/img/yellow_lamp.png'
+const clickButton = document.getElementById("click");
 
 function changeImage () {
     lampImg.src = imgPath;
     lampImg.alt = 'yellow_lamp';
 }
+
+clickButton.addEventListener('click', changeImage);
